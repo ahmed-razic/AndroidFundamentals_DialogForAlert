635f20577507fef26d63247f0fbeef7a0e76996a
ahmed-razic <ahmed.razic@gmail.com>
2021-02-10 13:04:04 +0100

    finished

--- app/src/main/java/com/example/android/androidfundamentals_dialogforalert/MainActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_dialogforalert/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.androidfundamentals_dialogforalert;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(MainActivity.this);
+
+        myAlertBuilder.setTitle("Alert");
+        myAlertBuilder.setMessage("Click OK to continue, or Cancel to stop:");
+
+        myAlertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), R.string.pressed_OK, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myAlertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), R.string.cancel, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myAlertBuilder.show();
     }
 }
